9f7bf26656503d29b553c5d7a88649af7bf39235
afazio1 <alexa.fazio04@gmail.com>
2024-04-05 23:12:28 -0400

    add db schemas

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import dbConnect from "@/server/db/dbConnect";
 import type { NextApiRequest, NextApiResponse } from "next";
-
+import User from "@/server/db/models/User";
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
+  
   res.status(200).json({ name: "John Doe" });
 }
