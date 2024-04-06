1ecda9f238bbc84c3c0d019635cfacf2d829ffbe
Yerti <36551149+UZ9@users.noreply.github.com>
2024-04-06 18:56:27 -0400

    Add ReactFlowWrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Sidebar from "@/components/Sidebar";
 
 import { Nunito as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
+import ReactFlowWrapper from "@/components/ReactFlowWrapper";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -67,6 +68,7 @@ export default function Home() {
     <div
       className={`${fontSans.variable} flex h-screen bg-background gap-4 justify-center items-center font-sans`}
     >
+      <ReactFlowWrapper/>
       <Sidebar courses={courses} />
       <Semester title={"Fall 24"} courses={courses} />
       <Semester title={"Spring 24"} courses={courses} />
