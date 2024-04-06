f85d85f6491322fae5bc8f20518eabc5b91caf93
Parker Abramson <parkerrabramson@gmail.com>
2024-04-06 12:34:06 -0400

    Add credits to course components

--- src/components/Course.tsx
+++ src/components/Course.tsx
@@ -12,29 +12,37 @@ export default function Course({ category }: CourseProps) {
     >
       {category.category ?
         <div>
-          <div className="mb-2">
+          <div className="text-xl text-primary-text mb-2">
             {category.category}
           </div>
           <div className="flex flex-col gap-2">
             {category.courses.map((course) => {
               return (
-                <div className="flex justify-center align-center bg-secondary-background rounded-[10px] gap-2 py-2">
-                  <div className="flex h-6 w-6 bg-secondary-color  border-2 border-primary-color rounded-full justify-center items-start">
-                    {category.selectedCourse === course.number ?
-                      <div className="h-6 w-6 stroke-background stroke-2">
-                        <CheckIcon />
-                      </div>
-                    : null }
+                <div className="flex justify-between align-center bg-secondary-background rounded-[10px] p-2">
+                  <div className="flex gap-2 text-priamry-text text-lg">
+                    <div className="flex h-6 w-6 bg-secondary-color border-2 border-primary-color rounded-full justify-center items-start">
+                      {category.selectedCourse === course.number ?
+                        <div className="h-6 w-6 stroke-background stroke-2">
+                          <CheckIcon />
+                        </div>
+                      : null }
+                    </div>
+                    <div className="text-primary-text text-sm">
+                      {course.number}
+                    </div>
                   </div>
-                  {course.number}
+                  <div className="text-sm text-secondary-text">
+                    {course.credits}
+                  </div>
                 </div>
               )
             })}
           </div>
         </div>
         :
-        <div>
-          {category["courses"][0]["number"]}...{category["courses"][0]["credits"]}
+        <div className="flex text-primary-text text-lg justify-between px-2">
+          <div>{category["courses"][0]["number"]}</div>
+          <div className="text-secondary-text">{category["courses"][0]["credits"]}</div>
         </div>
       }
     </button>
